789fa723aa41a077dad98837c1fbd6d97cee34c5
Nacho <ignacio.rodriguez@evoluciona.com>
2024-08-27 23:04:57 +0200

    no message

--- src/components/horarios/items.js
+++ src/components/horarios/items.js
@@ -19,54 +19,54 @@ const imageUrls = [
 ];
 
 const items = [
-    {
-      title: "Inicio",
-      description: "AQUI EXPLICO COSAS",
-      buttonText: "Inicio",
-      bgImage: inicial
-    },
-    {
-      title: "Preboda",
-      description: "Los novios se preparan para el gran día, ajustando los últimos detalles y compartiendo momentos especiales con sus seres queridos.",
-      buttonText: "Inicio",
-      bgImage: preboda
-    },
-    {
-      title: "Ceremonia",
-      description: "La ceremonia nupcial donde los novios intercambian sus votos, rodeados de amigos y familiares en un ambiente lleno de emoción.",
-      buttonText: "Botón 1",
-      bgImage: ceremonia
-    },
-    {
-      title: "Cóctel",
-      description: "Después de la ceremonia, los invitados disfrutan de un cóctel refrescante mientras los novios se toman un respiro para las fotos.",
-      buttonText: "Botón 2",
-      bgImage: coctel
-    },
-    {
-      title: "Comida",
-      description: "Un banquete delicioso es servido para todos los presentes, celebrando la unión de los novios con comida y bebida exquisitas.",
-      buttonText: "Botón 3",
-      bgImage: comida
-    },
-    {
-      title: "Baile",
-      description: "El baile de los recién casados abre la pista, seguido de una noche de música y diversión para todos los invitados.",
-      buttonText: "Botón 4",
-      bgImage: baile
-    },
-    {
-      title: "3er Tiempo",
-      description: "Un momento para relajarse después de las celebraciones principales, disfrutando de una charla y de bebidas en un ambiente más tranquilo.",
-      buttonText: "¿?",
-      bgImage: tercerTiempo
-    },
-    {
-      title: "Domingo",
-      description: "La fiesta culmina con una gran paellada, donde los invitados se despiden del evento disfrutando de un último plato tradicional.",
-      buttonText: "Final",
-      bgImage: paella
-    }
+  {
+    title: "Inicio",
+    description: <div>AQUI EXPLICO COSAS</div>,
+    buttonText: "Inicio",
+    bgImage: inicial
+  },
+  {
+    title: "Preboda",
+    description: <p>Los novios se preparan para el gran día, ajustando los últimos detalles y compartiendo momentos especiales con sus seres queridos.</p>,
+    buttonText: "Inicio",
+    bgImage: preboda
+  },
+  {
+    title: "Ceremonia",
+    description: <p>La ceremonia nupcial donde los novios intercambian sus votos, rodeados de amigos y familiares en un ambiente lleno de emoción.</p>,
+    buttonText: "Botón 1",
+    bgImage: ceremonia
+  },
+  {
+    title: "Cóctel",
+    description: <p>Después de la ceremonia, los invitados disfrutan de un cóctel refrescante mientras los novios se toman un respiro para las fotos.</p>,
+    buttonText: "Botón 2",
+    bgImage: coctel
+  },
+  {
+    title: "Comida",
+    description: <p>Un banquete delicioso es servido para todos los presentes, celebrando la unión de los novios con comida y bebida exquisitas.</p>,
+    buttonText: "Botón 3",
+    bgImage: comida
+  },
+  {
+    title: "Baile",
+    description: <p>El baile de los recién casados abre la pista, seguido de una noche de música y diversión para todos los invitados.</p>,
+    buttonText: "Botón 4",
+    bgImage: baile
+  },
+  {
+    title: "3er Tiempo",
+    description: <p>Un momento para relajarse después de las celebraciones principales, disfrutando de una charla y de bebidas en un ambiente más tranquilo.</p>,
+    buttonText: "¿?",
+    bgImage: tercerTiempo
+  },
+  {
+    title: "Domingo",
+    description: <p>La fiesta culmina con una gran paellada, donde los invitados se despiden del evento disfrutando de un último plato tradicional.</p>,
+    buttonText: "Final",
+    bgImage: paella
+  }
   ];
 
   const renderItems = () => {
@@ -75,7 +75,7 @@ const items = [
       <div className="info">
         <h2>{item.title}</h2>
         <div className="texts">
-          <p>{item.description}</p>
+          {item.description}
         </div>
         <button>{item.buttonText}</button>
       </div>
