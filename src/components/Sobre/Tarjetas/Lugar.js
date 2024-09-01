11e44028940cfb0272e2b6566c9b291fef4c9633
Nacho <ignacio.rodriguez@evoluciona.com>
2024-09-01 15:09:03 +0200

    no message

--- src/components/Sobre/Tarjetas/Lugar.js
+++ src/components/Sobre/Tarjetas/Lugar.js
@@ -4,12 +4,12 @@ const Lugar = () => {
     return (
         <div className="lugar seccion">
             <p>Todos los eventos tendrán lugar en Villas de Pomar</p>
-            <a href="https://maps.app.goo.gl/whoswYpUbrrjaCkm7" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://maps.app.goo.gl/whoswYpUbrrjaCkm7" target="_blank" rel="noopener noreferrer"> */}
               <img src={mapa} alt="Mapa del lugar" />
-            </a>
-            <a href="https://maps.app.goo.gl/whoswYpUbrrjaCkm7" target="_blank" rel="noopener noreferrer">
+            {/* </a> */}
+            {/* <a href="https://maps.app.goo.gl/whoswYpUbrrjaCkm7" target="_blank" rel="noopener noreferrer"> */}
                 <em>(Pedrosillo el Ralo, Salamanca)</em>
-            </a>
+            {/* </a> */}
             
         </div>
     );
