0a708dd51e087f1ce1f7b86ee531eae717f72265
Nacho <ignacio.rodriguez@evoluciona.com>
2024-09-04 14:11:34 +0200

    no message

--- src/components/Sobre/Tarjetas/Regalo.js
+++ src/components/Sobre/Tarjetas/Regalo.js
@@ -2,11 +2,17 @@ import "./Regalo.scss";
 const Regalo = () => {
     return (
         <div className="regalo seccion">
-            <p>Links a nuestros bizum</p>
 
-            <em>Si alguien cercano piensa en no asistir a la boda por cuestiones económicas, por favor, que nos lo diga en confianza.</em>
-            <em>Querremos que estés.</em>
             
+            <a href="ruta_del_primer_bizum">
+                <img src="ruta_de_la_primera_imagen" alt="Imagen 1" />
+            </a>
+            <a href="ruta_del_segundo_bizum">
+                <img src="ruta_de_la_segunda_imagen" alt="Imagen 2" />
+            </a>
+
+            <em>Si alguien se plantea no asistir a la boda por cuestiones económicas, por favor, que nos lo diga en confianza.</em>
+            <em>Querremos que estés.</em>
         </div>
     );
 }
