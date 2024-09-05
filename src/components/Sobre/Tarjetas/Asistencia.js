1f9c59b2aed9bbc033bd0e045cefde4d0238ecb9
Nacho <ignacio.rodriguez@evoluciona.com>
2024-09-05 18:26:19 +0200

    no message

--- src/components/Sobre/Tarjetas/Asistencia.js
+++ src/components/Sobre/Tarjetas/Asistencia.js
@@ -6,6 +6,7 @@ import OsitoBox from "../../OsitoBox/OsitoBox";
 const Asistencia = () => {
 
     const { activeCard, setActiveCard } = useDragContext();
+    const [ confirmacion, setConfirmacion ] = useState(true);
 
     const [formData, setFormData] = useState({
         nombre: "",
@@ -15,6 +16,7 @@ const Asistencia = () => {
     });
 
     const handleChange = (e) => {
+        console.log(e.target.checked);
         setFormData({
             ...formData,
             [e.target.name]: e.target.value,
@@ -35,7 +37,7 @@ const Asistencia = () => {
         const numeroTelefono = enviarANacho ? "679692422" : "626767895"; // Nacho o Mario
 
         // Crear el mensaje con saltos de línea
-        const mensaje = `Hola, soy ${nombre}.\nConfirmo mi asistencia: ${asistencia}.\nComentarios: ${comentarios}`;
+        const mensaje = `Hola, soy ${nombre}.\nConfirmo mi asistencia: ${confirmacion ? "Sí" : "No"}.\nComentarios: ${comentarios}`;
 
         // Construir la URL de WhatsApp
         const urlWhatsApp = `https://wa.me/34${numeroTelefono}?text=${encodeURIComponent(mensaje)}`;
@@ -46,11 +48,18 @@ const Asistencia = () => {
 
     return (<>
         <div className="asistencia seccion">
-            <p>Confirma tu asistencia</p>
-            <OsitoBox />
-            {/* <form>
+            
+            <form>
                 <div className="form-group">
-                    <label htmlFor="nombre">Nombre:</label>
+                    {/* <label htmlFor="asistencia">¿Asistirás?:</label> */}
+                    <OsitoBox                         
+                        // checked={formData.asistencia}
+                        confirmacion={confirmacion}
+                        setConfirmacion={setConfirmacion}
+                    />
+                </div>
+                <div className="form-group">
+                    <h2><label htmlFor="nombre">Nombre:</label></h2>
                     <input 
                         type="text" 
                         id="nombre" 
@@ -61,41 +70,27 @@ const Asistencia = () => {
                     />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="asistencia">¿Asistirás?:</label>
-                    <select 
-                        id="asistencia" 
-                        name="asistencia" 
-                        value={formData.asistencia}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="">Selecciona una opción</option>
-                        <option value="Sí">Sí</option>
-                        <option value="No">No</option>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="comentarios">Comentarios:</label>
+                <h2><label htmlFor="nombre">Comentarios:</label></h2>
                     <textarea 
                         id="comentarios" 
                         name="comentarios" 
                         value={formData.comentarios}
                         onChange={handleChange}
+                        placeholder="Si eres vegano, inviegno, alérgico o necesitas aclarar cualquier cosa, hazlo aquí. "
                     />
                 </div>
-                <div className="form-group">
-                    <label>Enviar a:</label>
-               
+                <div className="form-group horizontal quien">
+                    <label><h4>Enviar a:</h4></label>
                     <div className="switch">
                         <label>
-                            Mario
+                            <em>Mario</em>
                             <input 
                                 type="checkbox" 
                                 checked={formData.enviarANacho}
                                 onChange={handleSwitchChange}
                             />
                             <span className="slider"></span>
-                            Nacho
+                            <em>Nacho</em>
                         </label>
                     </div>
                 </div>
@@ -104,9 +99,9 @@ const Asistencia = () => {
                     className="btn-enviar" 
                     onClick={handleSendWhatsApp}
                 >
-                    <em>Confirmar y Enviar por WhatsApp</em>
+                    <em>Enviar por WhatsApp</em>
                 </button>
-            </form> */}
+            </form>
         </div>
         <button className="back" onClick={() => setActiveCard("home")} />
     </>);
