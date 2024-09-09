5142e46289dd32954695f57625713903b959a394
Nacho <ignacio.rodriguez@evoluciona.com>
2024-09-10 01:21:33 +0200

    no message

--- src/components/Backgrounds/Espiral/Espiral.js
+++ src/components/Backgrounds/Espiral/Espiral.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Espiral.scss"; // Asegúrate de importar el SCSS correctamente
+import "./Espiral.scss";
 
 const Espiral = () => {
   const [paused, setPaused] = useState(false);
@@ -8,28 +8,32 @@ const Espiral = () => {
     setPaused(!paused);
   };
 
-  // Generador de estrellas SVG (simplificado y corregido)
+  const generateStarSVG = () => `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+      <polygon points="12,2 15,8 22,8 17,12 19,19 12,15 5,19 7,12 2,8 9,8" fill="#c8102e" />
+    </svg>
+  `;
+
   const generateStars = (numberOfStars) => {
-    const starSVG = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-        ${Array.from({ length: numberOfStars }).map((_, i) => `
-          <polygon points="12,2 15,8 22,8 17,12 19,19 12,15 5,19 7,12 2,8 9,8" fill="#c8102e" transform="translate(${i * 4}px, ${i * 4}px)" />
-        `).join('')}
-      </svg>
-    `;
-    return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(starSVG)}`;
+    return Array.from({ length: numberOfStars }).map(() => `
+      <div class="star">
+        ${generateStarSVG()}
+      </div>
+    `).join('');
   };
 
   return (
     <>
-      <div className="espiral">
+      <div className={`espiral ${paused ? 'paused' : ''}`}>
         {Array.from({ length: 7 }, (_, i) => (
-          <i
+          <div
             key={i}
-            style={{ 
-              backgroundImage: `url(${generateStars(i + 1)})` // Añadimos estrellas como fondo
-            }}
-          />
+            className="bola"
+          >
+            <div className="star-container"
+              dangerouslySetInnerHTML={{ __html: generateStars(i + 1) }} // Añadimos estrellas como contenido HTML
+            />
+          </div>
         ))}
       </div>
       <div className="button-container">
@@ -37,13 +41,6 @@ const Espiral = () => {
           {paused ? "Reanudar Animación" : "Pausar Animación"}
         </button>
       </div>
-      <style>
-        {`
-          .espiral i {
-            animation-play-state: ${paused ? 'paused' : 'running'};
-          }
-        `}
-      </style>
     </>
   );
 };
