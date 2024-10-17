61f76b5ebcf955fb072d0ef16264191a7df7da13
RANMA\www <ignaciors86@gmail.com>
2024-10-17 23:43:44 +0200

    no message

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,13 +1,12 @@
 import gsap from "gsap";
-import { useEffect } from "react";
 
 let canAnimate = true; // Controla si la animación puede ejecutarse
 
 const animateOpacity = () => {
     // Si la animación no está disponible, evita ejecutar la función
     if (!canAnimate) {
-      // console.log("La animación no está disponible aún.");
-      return;
+        // console.log("La animación no está disponible aún.");
+        return;
     }
 
     // console.log("Ejecutando animación...");
@@ -15,50 +14,48 @@ const animateOpacity = () => {
     const tlInicial = gsap.timeline();
     // Obtiene la duración de la animación desde CSS (personalizable vía variables CSS)
     const duracion = getComputedStyle(document.documentElement)
-      .getPropertyValue('--duration')
-      .trim()
-      .replace('s', '') * 1; // Elimina la "s" del valor de segundos
+        .getPropertyValue('--duration')
+        .trim()
+        .replace('s', '') * 1; // Elimina la "s" del valor de segundos
 
     // Definir la animación utilizando la librería GSAP
     // console.log(canAnimate);
     if (canAnimate) {
-      tlInicial
-        .to("#myCanvas", {
-            opacity: 0.2,
-            duration: 1,
-            delay: 0,
-            ease: "ease",
-            
-        }, 0)
-        .to("body", { background: "cadetblue", duration: duracion * .5 }, ">")
-        .to(".sobre", { opacity: 0, zIndex: 2, duration: 0, scale: 0.7 }, ">")
-        .to(".prompt", { opacity: 0, duration: 0 }, ">")
-        
-        .to(".bubu-dudu", {
-          duration: 0,
-          ease: "ease-in",
-        }, ">")
-        
-
-        .to(".sobre", { opacity: 1, duration: duracion *2, scale: 1, y: 0, ease: "ease", }, ">")
-        .to(".bubu-dudu", {
-          rotateY: 90,
-          duration: duracion * 1,
-          ease: "ease-out",
-        }, ">")
-
-        .to(".envelope", {
-          rotateY: 0,
-          duration: duracion * 1,
-          ease: "ease-in",
-          onComplete: () => {
-            canAnimate = false; // canAnimate = false; // Evitar que la animación se ejecute nuevamente hasta que esté lista
-            // console.log("Animación completada.");
-          }
-        }, "<")
-
-        
+        tlInicial
+            .to("#myCanvas", {
+                opacity: 0.2,
+                duration: 1,
+                delay: 0,
+                ease: "ease",
+            }, 0)
+            .to("body", { background: "cadetblue", duration: duracion * .5 }, ">")
+            .to(".sobre", { opacity: 0, zIndex: 2, duration: 0, scale: 0.7 }, ">")
+            .to(".prompt", { opacity: 0, duration: 0 }, ">")
+            .to(".bubu-dudu", {
+                duration: 0,
+                ease: "ease-in",
+            }, ">")
+            .to(".sobre", { opacity: 1, duration: duracion * 2, scale: 1, y: 0, ease: "ease" }, ">")
+            .to(".bubu-dudu", {
+                rotateY: 90,
+                duration: duracion * 1,
+                ease: "ease-out",
+            }, ">")
+            .to(".envelope", {
+                rotateY: 0,
+                duration: duracion * 1,
+                ease: "ease-in",
+                onComplete: () => {
+                    canAnimate = false; // Evitar que la animación se ejecute nuevamente hasta que esté lista
+                    // console.log("Animación completada.");
+                }
+            }, "<");
     }
-  };
+};
+
+// Función para restablecer la animación
+export const resetAnimation = () => {
+    canAnimate = true; // Restablece el control de animación para permitir una nueva ejecución
+};
 
 export default animateOpacity;
