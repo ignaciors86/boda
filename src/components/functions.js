690f6c676a9db224263bd03bdbd48e68fbc7a275
RANMA\www <ignaciors86@gmail.com>
2024-10-17 22:53:54 +0200

    Reapply "no message"
    This reverts commit f454eb4bc4005960ae422a1c57b6a0b278802ad6.

--- src/components/functions.js
+++ src/components/functions.js
@@ -6,11 +6,11 @@ let canAnimate = true; // Controla si la animación puede ejecutarse
 const animateOpacity = () => {
     // Si la animación no está disponible, evita ejecutar la función
     if (!canAnimate) {
-      console.log("La animación no está disponible aún.");
+      // console.log("La animación no está disponible aún.");
       return;
     }
 
-    console.log("Ejecutando animación...");
+    // console.log("Ejecutando animación...");
 
     const tlInicial = gsap.timeline();
     // Obtiene la duración de la animación desde CSS (personalizable vía variables CSS)
@@ -20,7 +20,7 @@ const animateOpacity = () => {
       .replace('s', '') * 1; // Elimina la "s" del valor de segundos
 
     // Definir la animación utilizando la librería GSAP
-    console.log(canAnimate);
+    // console.log(canAnimate);
     if (canAnimate) {
       tlInicial
         .to("#myCanvas", {
@@ -53,7 +53,7 @@ const animateOpacity = () => {
           ease: "ease-in",
           onComplete: () => {
             canAnimate = false; // canAnimate = false; // Evitar que la animación se ejecute nuevamente hasta que esté lista
-            console.log("Animación completada.");
+            // console.log("Animación completada.");
           }
         }, "<")
 
