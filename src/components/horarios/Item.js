5000d7f510e159a55b1fec98a188868c28784fb1
RANMA\www <ignaciors86@gmail.com>
2024-10-18 02:46:19 +0200

    no message

--- src/components/horarios/Item.js
+++ src/components/horarios/Item.js
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
 import './Item.scss';
-const Item = ({data, index}) => {
-    // console.log(index);
-    return <div key={index} className={`item item${(parseInt(index) + 1)} ${data?.title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`}>
-        <div className="info">
-          <h2>{data?.title}</h2>
-          <div className="texts">
-            {data?.description}
-          </div>  
+
+const Item = ({ data, index }) => {
+    // Estado para mantener la imagen de fondo actual
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        // Configurar un intervalo para cambiar la imagen cada 2 segundos
+        const interval = setInterval(() => {
+            setCurrentImageIndex(prevIndex => (prevIndex + 1) % data.images.length);
+        }, 2000); // Cambiar cada 2 segundos
+
+        return () => clearInterval(interval); // Limpiar el intervalo al desmontar
+    }, [data.images.length]);
+
+    return (
+        <div 
+            key={index} 
+            className={`item item${(parseInt(index) + 1)} ${data?.title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`} 
+            style={{ backgroundImage: `url(${data.images[currentImageIndex]})` }} // Usar la imagen actual como fondo
+        >
+            <div className="info">
+                <h2>{data?.title}</h2>
+                <div className="texts">
+                    {data?.description}
+                </div>  
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default Item;
--- src/components/horarios/items.js
+++ src/components/horarios/items.js
@@ -1,80 +1,80 @@
-import preboda from "./assets/images/preboda.jpg";
-import ceremonia from "./assets/images/ceremonia.webp";
-import coctel from "./assets/images/coctel.jpg";
-import comida from "./assets/images/comida.jpg";
-import baile from "./assets/images/baile.webp";
-import tercerTiempo from "./assets/images/tercerTiempo.webp";
-import paella from "./assets/images/paella.jpg";
 import inicial from "./assets/images/ositos-drag.png";
+
+import theoffice from "./assets/images/ceremonia/theoffice.webp"; 
+import shameless from "./assets/images/ceremonia/shameless.webp"; 
+import monkeyisland from "./assets/images/ceremonia/monkeyisland.jpg";  // Asegúrate de que estas rutas sean correctas
+import monkeyisland2 from "./assets/images/ceremonia/monkeyisland2.jpg";  // Asegúrate de que estas rutas sean correctas
+import coctel from "./assets/images/coctel.jpg";  // Asegúrate de que estas rutas sean correctas
+import comida from "./assets/images/comida.jpg";  // Asegúrate de que estas rutas sean correctas
+import gilmoreBaile from "./assets/images/baile/gilmore.webp";  // Asegúrate de que estas rutas sean correctas
+import tercerTiempo from "./assets/images/tercerTiempo.webp";  // Asegúrate de que estas rutas sean correctas
+import paella from "./assets/images/paella.jpg";  // Asegúrate de que estas rutas sean correctas
+
 import Item from './Item.js';
 
 const imageUrls = [
     inicial,
-    preboda,
-    ceremonia,
+    theoffice,
+    shameless,
+    monkeyisland,
+    monkeyisland2,
     coctel,
     comida,
-    baile,
+    gilmoreBaile,
     tercerTiempo,
     paella,
 ];
 
 const items = [
-  {
-    title: "Agenda del finde",
-    description: <div><p>Aquí, además de explicar cosas a modo de resumen (si veo que me interesa) pondré una imagen con el cronograma resumido, para que se pueda ver de un vistazo</p></div>,
-    bgImage: inicial
-  },
-  {
-    title: "20:00: Preboda",
-    description: <>
-    {/* <p>
-      El viernes podéis pasaros por el sitio donde será la boda para una previa informal. 
-    </p> */}
-    <p>
-      Traete el bañador, y si tocas algún instrumento y te apetece tenerlo a mano para hacer el bobo un rato, adelante. 
-    </p>
-    </>,
-    bgImage: preboda
-  },
-  {
-    title: "13:00: Ceremonia",
-    description: <p>Palabras bonitas serán dichas y lloraremos todos mucho.</p>,
-    bgImage: ceremonia
-  },
-  {
-    title: "14:00: Cóctel",
-    description: <p>Ponte hasta el culo, por favor.</p>,
-    bgImage: coctel
-  },
-  {
-    title: "15:00: Comida",
-    description: <p>Un banquete delicioso es servido para todos los presentes, celebrando la unión de los novios con comida y bebida exquisitas.</p>,
-    bgImage: comida
-  },
-  {
-    title: "Baile",
-    description: <p>El baile de los recién casados abre la pista, seguido de una noche de música y diversión para todos los invitados.</p>,
-    bgImage: baile
-  },
-  {
-    title: "3er Tiempo",
-    description: <p>Un momento para relajarse después de las celebraciones principales, disfrutando de una charla y de bebidas en un ambiente más tranquilo.</p>,
-    bgImage: tercerTiempo
-  },
-  {
-    title: "Domingo",
-    description: <p>La fiesta culmina con una gran paellada, donde los invitados se despiden del evento disfrutando de un último plato tradicional.</p>,
-    bgImage: paella
-  }
+    {
+        title: "Agenda del finde",
+        description: <div><p>Aquí, además de explicar cosas a modo de resumen (si veo que me interesa) pondré una imagen con el cronograma resumido, para que se pueda ver de un vistazo</p></div>,
+        images: [inicial]
+    },
+    {
+        title: "20:00: Preboda",
+        description: <>
+            <p>Traete el bañador, y si tocas algún instrumento y te apetece tenerlo a mano para hacer el bobo un rato, adelante.</p>
+        </>,
+ images: [monkeyisland, monkeyisland2, comida]
+    },
+    {
+        title: "13:00: Ceremonia",
+        description: <p>Palabras bonitas serán dichas y lloraremos todos mucho.</p>,
+        
+        images: [theoffice, gilmoreBaile, monkeyisland, monkeyisland2, shameless]
+    },
+    {
+        title: "14:00: Cóctel",
+        description: <p>Ponte hasta el culo, por favor.</p>,
+        images: [coctel]
+    },
+    {
+        title: "15:00: Comida",
+        description: <p>Un banquete delicioso es servido para todos los presentes, celebrando la unión de los novios con comida y bebida exquisitas.</p>,
+        images: [comida]
+    },
+    {
+        title: "Baile",
+        description: <p>El baile de los recién casados abre la pista, seguido de una noche de música y diversión para todos los invitados.</p>,
+        images: [gilmoreBaile, monkeyisland2]
+    },
+    {
+        title: "3er Tiempo",
+        description: <p>Un momento para relajarse después de las celebraciones principales, disfrutando de una charla y de bebidas en un ambiente más tranquilo.</p>,
+        images: [tercerTiempo]
+    },
+    {
+        title: "Domingo",
+        description: <p>La fiesta culmina con una gran paellada, donde los invitados se despiden del evento disfrutando de un último plato tradicional.</p>,
+        images: [paella]
+    }
 ];
 
-
-  const renderItems = () => {
-    // console.log(items);
+const renderItems = () => {
     return items.map((item, index) => {
-      return item && <Item data={item} index={index} key={"item"+index} />
+        return item && <Item data={item} index={index} key={"item" + index} />;
     });
-  };
+};
 
-  export { items, imageUrls, renderItems };
+export { items, imageUrls, renderItems };
