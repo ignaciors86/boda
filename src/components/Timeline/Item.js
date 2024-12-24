6e262162418b7641b9ef90d5a8f2f25d88787c2d
Nacho <ignacio.rodriguez@evoluciona.com>
2024-12-24 14:06:25 +0100

    no message

--- src/components/Timeline/Item.js
+++ src/components/Timeline/Item.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react';
 import './Item.scss';
 
 const Item = ({ data, index }) => {
-    // Estado para mantener la imagen de fondo actual
+    // Estado para mantener la imagen actual visible
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
-        // Configurar un intervalo para cambiar la imagen cada 2 segundos
+        // Configurar un intervalo para cambiar la imagen visible cada cierto tiempo
         const interval = setInterval(() => {
             setCurrentImageIndex(prevIndex => (prevIndex + 1) % data.images.length);
-        }, index === 0 ? 500 : 2500); // Cambiar cada 2 segundos
+        }, index === 0 ? 500 : 2500);
 
         return () => clearInterval(interval); // Limpiar el intervalo al desmontar
     }, [data.images.length]);
@@ -17,9 +17,23 @@ const Item = ({ data, index }) => {
     return (
         <div 
             key={index} 
-            className={`item item${(parseInt(index) + 1)} ${data?.title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`} 
-            style={{ backgroundImage: `url(${data.images[currentImageIndex]})` }} // Usar la imagen actual como fondo
+            style={{ backgroundImage: `url(${data.images[currentImageIndex]})`,
+         }} // Usar la imagen actual como fondo
+            className={`item item${(parseInt(index) + 1)} ${data?.title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`}
+         
         >
+            <div className="images-container"
+                style={{ backgroundImage: `url(${data.images[currentImageIndex]})` }} // Usar la imagen actual como fondo
+            >
+                {data.images.map((image, imgIndex) => (
+                    <img 
+                        key={imgIndex} 
+                        src={image} 
+                        alt={`Slide ${imgIndex}`} 
+                        className={`image ${currentImageIndex === imgIndex ? 'visible' : 'hidden'}`}
+                    />
+                ))}
+            </div>
             <div className="info">
                 <h2>{data?.title}</h2>
                 <div className="texts">
@@ -28,6 +42,6 @@ const Item = ({ data, index }) => {
             </div>
         </div>
     );
-}
+};
 
 export default Item;
