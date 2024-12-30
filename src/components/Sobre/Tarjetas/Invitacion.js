4f2fa70edc3752598ed880da30b81b26c6bd840e
Nacho <ignacio.rodriguez@evoluciona.com>
2024-12-30 14:57:09 +0100

    no message

--- src/components/Sobre/Tarjetas/Invitacion.js
+++ src/components/Sobre/Tarjetas/Invitacion.js
@@ -3,8 +3,8 @@ import invitacionBack from '../assets/images/invitacion-back.jpg';
 const Invitacion = () => {
     return (
         <div className="invitacion seccion" style={{background: "url(" +  invitacionBack + ")"}}>
-            <p>Queremos invitarte a nuestra boda. No sabemos si será la del año o no, pero sí que queremos que estés.</p>
-            <em>En cada tarjeta tienes información relevante para ese día. Lo más importante es que confirmes tu asistencia lo antes posible</em>
+            <em>Queremos invitarte a nuestra boda. No sabemos si será la del año o no, pero sí que queremos que estés.</em>
+            <p>En cada tarjeta tienes información relevante para ese día. Lo más importante es que confirmes tu asistencia lo antes posible, pero antes, mira con calma las demás</p>
         </div>
     );
 }
