141f5862336934628ce3d7d8bf6e7ca7fcda082a
Nacho <ignacio.rodriguez@evoluciona.com>
2025-01-02 17:52:18 +0100

    no message

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -263,7 +263,7 @@ const Timeline = () => {
     hasInteracted && gsap.killTweensOf(sliderRef.current);
   }, [hasInteracted]);
 
-  return activeCard === "horarios" && <>
+  return <>
     <div className={`${MAINCLASS} seccion`}>
       <Loading />
       <div className="elements">
