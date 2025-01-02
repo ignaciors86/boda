e5ec5fad1b9887dc9d9b2d63588820976f2e73ef
Nacho <ignacio.rodriguez@evoluciona.com>
2025-01-02 11:26:12 +0100

    no message

--- src/components/Timeline/Loading.js
+++ src/components/Timeline/Loading.js
@@ -1,5 +1,21 @@
+import gsap from "gsap";
+import ositos from "./assets/images/ositos-drag.png";
+
 const Loading = () => {
-    return <div className="loading">Loading...</div>
+
+    gsap.to(".loading img", {
+        scale: 1.3,
+        repeat: -1,
+        yoyo: true,
+        duration: 0.5,
+        ease: "linear",
+    });
+
+    return <div className="loading">
+        <h2>Cargando... </h2>
+        <img src={ositos} alt="Ositos cargando" />
+        <em>Si estás usando una red lenta, ten paciencia.</em>
+    </div>
 }
 
 export default Loading;
