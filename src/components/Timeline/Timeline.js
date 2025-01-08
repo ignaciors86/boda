c7c3e73e2e8f8779a93d0e7887931546a581ec50
Nacho <ignacio.rodriguez@evoluciona.com>
2025-01-09 00:26:42 +0100

    no message

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -141,9 +141,9 @@ const play = () => {
             <div className="elements">{renderItems(currentIndex,  weedding || null)}</div>
             <Loading text={true} />
             <div className="progress-bar">
-              <Marquee speed={25}>
+              <Marquee speed={50}>
                 <span>
-                  Arrastra la bolita hacia los lados para ver bien el finde que hemos planeado. Al principio hay mucho gif y música en proceso de descarga y estoy usando un servidor gratuíto. Esto, al principio, provoca dolor de tripita en los iPhone{ weedding ? " (como las setas)" : ""}, y sí, probablemente podría estar más optimizado, pero peor sería morirse... 
+                  Arrastra la bolita hacia los lados para ver bien el finde que hemos planeado. Al principio hay mucho gif y música en proceso de descarga y estoy usando un servidor gratuíto. Esto, al principio, provoca dolor de tripita en los iPhone{ weedding ? " (como las setas)" : ""}, y sí, probablemente podría estar más optimizado, pero { !weedding ? "yo que se, algo tenía que malir sal..." : "me esta dando una pereza ya que flipas revisarlo más"} 
                 </span>
               </Marquee>
               <input
