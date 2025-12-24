18d9455a707fa071492de438e6b3bdbc39499b87
Nacho <ignacio.rodriguez@evoluciona.com>
2025-12-24 16:25:49 +0100

    no message

---
+++ src/components/Croquetas25/assets/tracks/croquetas26/guion.js
@@ -0,0 +1,41 @@
+// Guion para croquetas26
+// Humor estilo Broncano sobre reutilizar fotos de la boda
+export default {
+  textos: [
+    '2025: el año de las croquetas 26',
+    'Cuando reutilizas las fotos de tu boda',
+    'Que no pudiste usar para otra cosa',
+    'Para hacer esta frikada a última hora',
+    'Las fotos que te enviaron para la boda',
+    'Y que no servían para nada',
+    'Ahora son parte de esta experiencia',
+    'Porque no tenías tiempo para otra cosa',
+    'En 2025, reutilizas todo',
+    'Incluso las fotos que no usaste',
+    'Las fotos que te enviaron por WhatsApp',
+    'Y que no sabías qué hacer con ellas',
+    'Ahora están aquí',
+    'Porque necesitabas contenido rápido',
+    'Esta frikada se hizo a última hora',
+    'Con las fotos que tenías por ahí',
+    'Las fotos de la boda que no usaste',
+    'Porque no eran lo suficientemente buenas',
+    'Ahora son perfectas para esto',
+    'Porque aquí todo vale',
+    'En 2025, reutilizas las fotos',
+    'Que no pudiste usar en otro sitio',
+    'Y las conviertes en arte',
+    'O algo así',
+    'Las fotos que te enviaron',
+    'Y que no sabías dónde poner',
+    'Ahora están en esta experiencia',
+    'Porque no tenías otra opción',
+    'Esta frikada se hizo rápido',
+    'Con lo que había disponible',
+    'Las fotos de la boda que no usaste',
+    'Ahora son parte de esto',
+    'Porque necesitabas algo ya',
+    'Fin. O eso dices mientras reutilizas más fotos.'
+  ]
+};
+
